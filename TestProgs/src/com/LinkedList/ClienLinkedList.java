c59dec687e800c2f1c0cc7ed6332133e461bd551
viplav.jain <viplav.jain@DEL1-LHP-N61492.synapse.com>
2019-12-10 16:52:28 +0530

    Linked List operations

---
+++ TestProgs/src/com/LinkedList/ClienLinkedList.java
@@ -0,0 +1,14 @@
+package com.LinkedList;
+
+public class ClienLinkedList 
+{
+  public static void main(String[] args) 
+  {
+	 LinkedList ll = new LinkedList();
+	 ll.addLast(3);
+	 ll.addLast(7);
+	 ll.addLast(5);
+	 ll.addFirst(2);
+	 ll.display();
+  }
+}
---
+++ TestProgs/src/com/LinkedList/LinkedList.java
@@ -0,0 +1,69 @@
+package com.LinkedList;
+
+public class LinkedList<T>
+{
+	private class Node
+	{
+		int data;
+		Node next;
+	}
+	
+	private Node head;
+	private Node tail;
+	private int size;
+	
+	public void display()
+	{
+	  	Node temp=head;
+	  	while(temp!=null)
+	  	{
+	  		System.out.println(temp.data);
+	  		temp=temp.next;
+	  	}
+	}
+	
+	public void addLast(int item)
+	{
+		Node nn = new Node();
+		nn.data=item;
+		nn.next=null;
+		
+		if(this.size==0)
+		{
+		  head=nn;
+		  tail=nn;
+		}
+		
+		else
+		{
+			tail.next=nn;
+			tail=nn;
+			
+		}
+		size++;
+	  	
+	}
+	
+	public void addFirst(int item)
+	{
+		Node nn = new Node();
+		nn.data=item;
+		nn.next=null;
+		
+		if(this.size==0)
+		{
+		  head=nn;
+		  tail=nn;
+		}
+		
+		else
+		{ 
+		  nn.next=head;
+		  head=nn; 	
+		}
+		size++;
+		
+	}
+	
+
+}
